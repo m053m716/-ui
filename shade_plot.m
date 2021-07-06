3f1acee997ea94517a3d08ab825fa3993f398c80
Max-MI115 <max.murphy11@gmail.com>
2021-07-06 12:58:22 -0400

    Bugfix so shade_plot handles empty vectors.

--- shade_plot.m
+++ shade_plot.m
@@ -14,12 +14,29 @@ function h = shade_plot(ax, x, y, varargin)
 %
 % See also: Contents, patch
 
+c = validatecolor('#2b68d9');
+if isempty(x) || isempty(y)
+    h = patch('Faces', 1:2, 'Vertices', nan(2,2), 'Parent', ax, ...
+        'FaceAlpha', 0.33, ...
+        'FaceColor', c, ...
+        'EdgeColor', 'none', ...
+        varargin{:});
+    return;
+end
+
 x = reshape(x, numel(x), 1);
 y = reshape(y, numel(y), 1);
 data = [x, y];
 
 coeff = pca(data);
-
+if isempty(coeff)
+    h = patch('Faces', 1:2, 'Vertices', nan(2,2), 'Parent', ax, ...
+        'FaceAlpha', 0.33, ...
+        'FaceColor', c, ...
+        'EdgeColor', 'none', ...
+        varargin{:});
+    return;
+end
 T = ([0 -0.5; 0.5 0] * coeff(:, 1))'; % Rotate to perpendicular
 
 A = data + T;
@@ -27,7 +44,7 @@ B = flipud(data - T);
 V = [A; B];
 F = [1:size(V,1), 1];
 
-c = validatecolor('#2b68d9');
+
 h = patch('Faces', F, 'Vertices', V, ...
     'Parent', ax, ...
     'FaceAlpha', 0.33, ...
